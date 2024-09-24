db11cf24bcb298ca65867ff2ca709704177701d3
anandpanigrahy <138498343+anandpanigrahy@users.noreply.github.com>
2024-09-24 16:46:31 -0400

    - Converted function to utilize saemix for mixed-effects regression model - Changed error handling - Included while loop for iterative mixed-model fitting attempts

--- R/exponential_mixed_model.R
+++ R/exponential_mixed_model.R
@@ -1,38 +1,47 @@
 #' Fit an exponential mixed-effects regression model
 #'
 #' @description
-#' 'exponential_mixed_model()' is a function utilized with the \code{\link{growth_curve_model_fit}} function for fitting a mono-exponential mixed-effects regression model to growth data utilizing the \code{\link[nlme]{nlme}} function.
-#' Starting values are derived from an initial least-squares model using the \code{\link[minpack.lm]{nlsLM}} function.
+#' 'exponential_mixed_model()' is a function utilized with the
+#' \code{\link{growth_curve_model_fit}} function for fitting a mono-exponential
+#' mixed-effects regression model to growth data utilizing the saemix package.
+#' Starting values are derived from an initial least-squares model using
+#' the \code{\link[minpack.lm]{nlsLM}} function.
 #'
 #'
 #' @inheritParams growth_curve_model_fit
 #'
-#' @return Returns an exponential model object of class 'nlme' when a mixed-effects model is specified or a model object of class 'nls' if a least-squares model is specified.
+#' @return Returns an exponential model object of class 'saemix' when a
+#' mixed-effects model is specified or a model object of class 'nls' if a
+#' least-squares model is specified.
 #' @seealso \link{growth_curve_model_fit}
 #' @importFrom magrittr %>%
 #' @importFrom dplyr filter pull summarise
 #' @importFrom minpack.lm nlsLM
-#' @importFrom nlme nlme nlmeControl
-#' @importFrom stats na.omit
+#' @importFrom saemix saemixData saemixModel
 #' @export
 #'
 #' @examples
 #' # Load example data (exponential data from GrowthCurveME package)
 #' data(exp_mixed_data)
-#' # Fit an exponential mixed-effects growth model to the data using the main growth_curve_model_fit() function
-#' exp_mixed_model <- growth_curve_model_fit(data_frame = exp_mixed_data, function_type = "exponential")
-#' # Fit an exponential mixed-effected model using the exponential_mixed_model() function
+#' # Fit an exponential mixed-effects growth model to the data using the main
+#' growth_curve_model_fit() function
+#' exp_mixed_model <- growth_curve_model_fit(data_frame = exp_mixed_data,
+#' function_type = "exponential")
+#' # Fit an exponential mixed-effected model using the
+#' exponential_mixed_model() function
 #' exp_mixed_model <- exponential_mixed_model(data_frame = exp_mixed_data)
 exponential_mixed_model <- function(data_frame,
                                     model_type = "mixed",
-                                    fixed_rate = TRUE) {
+                                    fixed_rate = TRUE,
+                                    saemix_nb.chains = 1) {
   # Calculating starting value for intercept
   start_intercept <- data_frame %>%
     dplyr::filter(time == min(time)) %>%
     dplyr::summarise(mean = mean(growth_metric, na.rm = TRUE)) %>%
     dplyr::pull(mean)
-  # Fit an initial exponential model using the minpack.lm::nlsLM to get starting values for exponential mixed effects model
-  initial_exp_model <- tryCatch(
+  #   Fit an initial exponential model using the minpack.lm::nlsLM to get
+  # starting values for exponential mixed effects model
+  ls_model <- tryCatch(
     expr = {
       withCallingHandlers(
         minpack.lm::nlsLM(growth_metric ~ intercept * exp(rate * time),
@@ -50,80 +59,175 @@ exponential_mixed_model <- function(data_frame,
     error = function(e) {
       error_message <- paste("Caution an error occured: ", e)
       message(error_message)
-      stop("Initial non-linear least squares model could not be fit due to error, please inspect data or change function type")
     }
   )
+
+  # Obtain starting values for mixed model
+  if (inherits(ls_model, "nls")) {
+    # Extract model estimates from ls_model to use as starting values
+    start_intercept <- round(as.numeric(ls_model$m$getPars()[1]), 6)
+    start_rate <- round(as.numeric(ls_model$m$getPars()[2]), 6)
+
+    # Create summary object
+    sum_object <- summary(ls_model)
+
+    # Create sequence of other starting values
+    intercept_sd <- sum_object$coefficients[1, 2] * sqrt(nrow(data_frame))
+    rate_sd <- sum_object$coefficients[2, 2] * sqrt(nrow(data_frame))
+
+    set.seed(123)
+    start_intercept_vec <- runif(
+      n = 10,
+      min = start_intercept - (2 * intercept_sd),
+      max = start_intercept + (2 * intercept_sd)
+    )
+    start_rate_vec <- runif(
+      n = 10,
+      min = start_rate - (2 * rate_sd),
+      max = start_rate + (2 * rate_sd)
+    )
+
+    start_intercept_vec <- c(start_intercept, start_intercept_vec)
+    start_rate_vec <- c(start_rate, start_rate_vec)
+  } else {
+    stop(paste(
+      "Initial least-squares model did not converge,",
+      "function selected may not be appropriate for data"
+    ))
+  }
+
   # If model_type model is specified
   if (model_type == "mixed") {
-    # Extract model estimates from initial_exp_model to use as starting values
-    start_intercept <- round(as.numeric(initial_exp_model$m$getPars()[1]), 6)
-    start_rate <- round(as.numeric(initial_exp_model$m$getPars()[2]), 6)
+    # Prepare SAEMIX data object
+    saemix_data <- saemix::saemixData(
+      name.data = data_frame,
+      name.group = "cluster",
+      name.predictors = "time",
+      name.response = "growth_metric",
+      units = list(x = "time", y = "growth_metric"),
+      verbose = FALSE
+    )
+    # Create SAEMIX model function
+    saemix_function <- function(psi, id, x) {
+      time <- x[, 1]
+      intercept <- psi[id, 1]
+      rate <- psi[id, 2]
+
+      ypred <- intercept * exp(rate * time)
+
+      return(ypred)
+    }
+    # Set NLMEG options
+    NLMEG.options <- list(
+      seed = 1234, displayProgress = FALSE,
+      print = FALSE, save = FALSE,
+      save.graphs = FALSE,
+      nb.chains = saemix_nb.chains
+    )
+
     # If fixed_rate is TRUE
     if (fixed_rate == TRUE) {
-      # Fit non-linear (exponential) mixed model with random intercepts and fixed rates to the data
-      exp_model <- tryCatch(
-        expr = {
-          withCallingHandlers(
-            nlme::nlme(growth_metric ~ intercept * exp(rate * time),
-              method = "REML",
-              data = data_frame,
-              fixed = intercept + rate ~ 1,
-              random = intercept ~ 1,
-              groups = ~cluster,
-              start = c(
-                intercept = start_intercept,
-                rate = start_rate
-              ),
-              na.action = na.omit,
-              control = nlme::nlmeControl(maxIter = 5000)
+      model_saemix <- "No"
+      count <- 1
+      while (!inherits(model_saemix, "SaemixObject") &
+        count <= length(start_intercept_vec)) {
+        # Set model structure
+        saemix_model_object <- saemix::saemixModel(
+          model = saemix_function,
+          description = "SAEMIX Exponential Model",
+          psi0 = c(
+            intercept = start_intercept_vec[count],
+            rate = start_rate_vec[count]
+          ),
+          covariance.model = matrix(
+            c(
+              1, 0,
+              0, 0
             ),
-            warning = function(w) {
-              invokeRestart("muffleWarning")
-            }
-          )
-        },
-        error = function(e) {
-          error_message <- paste("Caution an error occured: ", e)
-          message(error_message)
-          stop("Initial fit of exponential mixed model failed, current model specification may be in-appropriate for provided data, please inspect data or change function type")
-        }
-      )
-      # If fixed_rate is FALSE
+            ncol = 2, byrow = TRUE
+          ),
+          transform.par = c(0, 0),
+          verbose = FALSE
+        )
+        # Fit mixed-effects model
+        model_saemix <- tryCatch(
+          expr = {
+            withCallingHandlers(
+              saemix(
+                model = saemix_model_object,
+                data = saemix_data,
+                control = NLMEG.options
+              ),
+              warning = function(w) {
+                invokeRestart("muffleWarning")
+              }
+            )
+          },
+          error = function(e) {
+            error_message <- paste("Caution an error occured: ", e)
+            message(error_message)
+            return("No")
+          }
+        )
+        count <- count + 1
+      }
     } else {
-      # Fit non-linear (exponential) mixed model with random intercepts and random rates to the data
-      exp_model <- tryCatch(
-        expr = {
-          withCallingHandlers(
-            nlme::nlme(growth_metric ~ intercept * exp(rate * time),
-              method = "REML",
-              data = data_frame,
-              fixed = intercept + rate ~ 1,
-              random = intercept + rate ~ 1,
-              groups = ~cluster,
-              start = c(
-                intercept = start_intercept,
-                rate = start_rate
-              ),
-              na.action = na.omit,
-              control = nlme::nlmeControl(maxIter = 5000)
+      model_saemix <- "No"
+      count <- 1
+      while (!inherits(model_saemix, "SaemixObject") &
+        count <= length(start_intercept_vec)) {
+        # Set model structure
+        saemix_model_object <- saemix::saemixModel(
+          model = saemix_function,
+          description = "SAEMIX Exponential Model",
+          psi0 = c(
+            intercept = start_intercept_vec[count],
+            rate = start_rate_vec[count]
+          ),
+          covariance.model = matrix(
+            c(
+              1, 1,
+              1, 1
             ),
-            warning = function(w) {
-              invokeRestart("muffleWarning")
-            }
-          )
-        },
-        error = function(e) {
-          error_message <- paste("Caution an error occured: ", e)
-          message(error_message)
-          stop("Initial fit of exponential mixed model failed, current model specification may be in-appropriate for provided data, please inspect data, change fixed_rate to TRUE, or change function type")
-        }
-      )
+            ncol = 2, byrow = TRUE
+          ),
+          transform.par = c(0, 0),
+          verbose = FALSE
+        )
+        # Fit mixed-effects model
+        model_saemix <- tryCatch(
+          expr = {
+            withCallingHandlers(
+              saemix(
+                model = saemix_model_object,
+                data = saemix_data,
+                control = NLMEG.options
+              ),
+              warning = function(w) {
+                invokeRestart("muffleWarning")
+              }
+            )
+          },
+          error = function(e) {
+            error_message <- paste("Caution an error occured: ", e)
+            message(error_message)
+            return("No")
+          }
+        )
+        count <- count + 1
+      }
     }
-    # Return the exponential model_type model
-    return(exp_model)
-    # If model_type model is not requested
+    if (inherits(model_saemix, "SaemixObject")) {
+      # Return the mixed-model
+      return(model_saemix)
+    } else {
+      stop(paste(
+        "Mixed-effects model did not converge,",
+        "function selected may not be appropriate for data"
+      ))
+    }
   } else {
-    # Return the exponential nls model
-    return(initial_exp_model)
+    # Return the ls model
+    return(ls_model)
   }
 }
